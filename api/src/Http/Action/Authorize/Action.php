b15a4c8ef282a2f08cb35bc8e2068339ffa003ab
vorozcovmaksim <myks1992@mail.ru>
2023-04-30 01:04:02 +0300

    Move Action

--- api/src/Http/Action/Authorize/Action.php
+++ api/src/Http/Action/Authorize/Action.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Action;
+namespace App\Http\Action\Authorize;
 
 use App\Http\Response\HtmlResponse;
 use App\Shared\Template\TemplateInterface;
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * @author Maksim Vorozhtsov <myks1992@mail.ru>
  */
 #[Route('/authorize', name: 'authorize', methods: ['GET'])]
-final readonly class AuthorizeAction
+final readonly class Action
 {
     public function __construct(
         private TemplateInterface $template
--- api/src/Http/Action/Home/Action.php
+++ api/src/Http/Action/Home/Action.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Action;
+namespace App\Http\Action\Home;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * @author Maksim Vorozhtsov <myks1992@mail.ru>
  */
 #[Route('', name: 'home', methods: ['GET'])]
-final class HomeAction
+final class Action
 {
     public function __invoke(): JsonResponse
     {
