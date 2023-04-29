aaa3ab4cf19592f27cd57bfc7cb5d631817e8475
vorozcovmaksim <myks1992@mail.ru>
2023-04-30 00:38:04 +0300

    Remove yaml config for Security

--- api/src/Security/config.php
+++ api/src/Security/config.php
@@ -10,7 +10,10 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 return static function (ContainerConfigurator $configurator): void {
     $configurator->parameters()
-        ->set('security.login_url', env('SECURITY_LOGIN_URL'));
+        ->set('security.login_url_default', 'http://localhost')
+        ->set('security.login_url', env('SECURITY_LOGIN_URL')->default('security.login_url_default'))
+        ->set('security.target_path_parameter_default', 'redirect_url')
+        ->set('security.target_path_parameter', env('SECURITY_TARGET_PATH_PARAMETER')->default('security.target_path_parameter_default'));
 
     $services = $configurator
         ->services()
@@ -22,4 +25,10 @@ return static function (ContainerConfigurator $configurator): void {
         './config.php',
         './Test',
     ]);
+
+    $services->set(RedirectUrlAuthenticationEntryPoint::class)
+        ->args([
+            '$loginUrl' => '%security.login_url%',
+            '$targetPathParameter' => '%security.target_path_parameter%',
+        ]);
 };
