76bab0dc5df8563c7fefefeabb1304dad95dc944
vorozcovmaksim <myks1992@mail.ru>
2023-05-08 18:45:33 +0300

    Move config

--- api/src/Shared/config.php
+++ api/src/Shared/config.php
@@ -4,25 +4,9 @@ declare(strict_types=1);
 
 namespace App\Shared;
 
-use App\Shared\Doctrine\Types\JsonUnescapedType;
-use App\Shared\Flusher\DoctrineFlusher;
-use App\Shared\Flusher\DomainEventDispatcherFlusher;
-use App\Shared\Flusher\FlusherInterface;
-use App\Shared\Flusher\FlushersFlusher;
-use Doctrine\DBAL\Types\Types as DoctrineTypes;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
-
 return static function (ContainerConfigurator $configurator): void {
-    $configurator->extension('doctrine', [
-        'dbal' => [
-            'types' => [
-                DoctrineTypes::JSON => JsonUnescapedType::class,
-            ],
-        ],
-    ]);
-
     $services = $configurator
         ->services()
         ->defaults()
@@ -32,14 +16,12 @@ return static function (ContainerConfigurator $configurator): void {
     $services->load('App\\Shared\\', '.')->exclude([
         './config.php',
         './*/Test',
+        './Doctrine',
+        './Flusher',
         './ValueObject',
         './Aggregate',
         './FeatureToggle',
         './Notifier',
         './Assert.php',
     ]);
-
-    $services->get(DomainEventDispatcherFlusher::class)->tag('flusher', ['priority' => -910]);
-    $services->get(DoctrineFlusher::class)->tag('flusher', ['priority' => -915]);
-    $services->set(FlusherInterface::class, FlushersFlusher::class)->args([tagged_iterator('flusher')]);
 };
