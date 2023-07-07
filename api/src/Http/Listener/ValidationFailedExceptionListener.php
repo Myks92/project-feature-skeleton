59d2310a715701656968e61653a9a6fcb0f0befe
Maksim Vorozhtsov <myks1992@mail.ru>
2023-07-07 17:09:30 +0300

    Add Converter for errors Symfony Validator

---
+++ api/src/Http/Listener/ValidationFailedExceptionListener.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Listener;
+
+use App\Shared\Validator\Error;
+use App\Shared\Validator\Errors;
+use App\Shared\Validator\ValidationException;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+
+/**
+ * @author Maksim Vorozhtsov <myks1992@mail.ru>
+ */
+#[AsEventListener(event: KernelEvents::EXCEPTION)]
+final class ValidationFailedExceptionListener
+{
+    public function __invoke(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if (!$exception instanceof ValidationFailedException) {
+            return;
+        }
+
+        $errors = [];
+        foreach ($exception->getViolations() as $violation) {
+            $errors[] = self::violationToError($violation);
+        }
+
+        throw new ValidationException(new Errors($errors));
+    }
+
+    private static function violationToError(ConstraintViolationInterface $violation): Error
+    {
+        return new Error($violation->getPropertyPath(), $violation->getMessage());
+    }
+}
---
+++ api/src/Http/Test/Listener/ValidationFailedExceptionListenerTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Test\Listener;
+
+use App\Contracts\Validator\Exception\ValidationFailed;
+use App\Http\Listener\ValidationFailedExceptionListener;
+use Exception;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+
+/**
+ * @internal
+ * @author Maksim Vorozhtsov <myks1992@mail.ru>
+ */
+#[CoversClass(ValidationFailedExceptionListener::class)]
+final class ValidationFailedExceptionListenerTest extends TestCase
+{
+    private EventDispatcher $dispatcher;
+
+    protected function setUp(): void
+    {
+        $this->dispatcher = new EventDispatcher();
+    }
+
+    public function testNotValidation(): void
+    {
+        $listener = new ValidationFailedExceptionListener();
+
+        $this->dispatcher->addListener(KernelEvents::EXCEPTION, $listener);
+
+        $event = new ExceptionEvent(
+            $this->createStub(HttpKernelInterface::class),
+            new Request([], [], [], [], [], ['CONTENT_TYPE' => 'application/json']),
+            HttpKernelInterface::MAIN_REQUEST,
+            new Exception('No validation exception.')
+        );
+
+        $this->dispatcher->dispatch($event, KernelEvents::EXCEPTION);
+
+        $response = $event->getResponse();
+
+        self::assertNull($response);
+    }
+
+    public function testValidation(): void
+    {
+        $listener = new ValidationFailedExceptionListener();
+
+        $this->dispatcher->addListener(KernelEvents::EXCEPTION, $listener);
+
+        $event = new ExceptionEvent(
+            $this->createStub(HttpKernelInterface::class),
+            new Request([], [], [], [], [], ['CONTENT_TYPE' => 'application/json']),
+            HttpKernelInterface::MAIN_REQUEST,
+            new ValidationFailedException(
+                'value',
+                new ConstraintViolationList([
+                    new ConstraintViolation('Incorrect email', null, [], null, 'email', 'not-email'),
+                    new ConstraintViolation('Empty password', null, [], null, 'password', ''),
+                ])
+            )
+        );
+
+        try {
+            $this->dispatcher->dispatch($event, KernelEvents::EXCEPTION);
+        } catch (ValidationFailed $exception) {
+            self::assertInstanceOf(ValidationFailed::class, $exception);
+            self::assertCount(2, $errors = $exception->getErrors()->getErrors());
+
+            self::assertSame('email', $errors[0]->getPropertyPath());
+            self::assertSame('Incorrect email', $errors[0]->getMessage());
+
+            self::assertSame('password', $errors[1]->getPropertyPath());
+            self::assertSame('Empty password', $errors[1]->getMessage());
+        }
+    }
+}
