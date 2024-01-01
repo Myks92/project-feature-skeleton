0a20b26016f2ec00ca0ea08f04eb4be190caaec4
Maksim Vorozhtsov <myks1992@mail.ru>
2024-01-01 22:45:54 +0300

    Remove RedirectUrlAuthenticationEntryPoint

--- api/src/Http/config.php
+++ api/src/Http/config.php
@@ -4,18 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http;
 
-use App\Http\Authentication\RedirectUrlAuthenticationEntryPoint;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
-
 return static function (ContainerConfigurator $configurator): void {
-    $configurator->parameters()
-        ->set('security.login_url_default', 'http://localhost')
-        ->set('security.login_url', env('SECURITY_LOGIN_URL')->default('security.login_url_default'))
-        ->set('security.target_path_parameter_default', 'redirect_url')
-        ->set('security.target_path_parameter', env('SECURITY_TARGET_PATH_PARAMETER')->default('security.target_path_parameter_default'));
-
     $services = $configurator
         ->services()
         ->defaults()
@@ -34,10 +25,4 @@ return static function (ContainerConfigurator $configurator): void {
         './Response',
         './Test',
     ]);
-
-    $services->set(RedirectUrlAuthenticationEntryPoint::class)
-        ->args([
-            '$loginUrl' => '%security.login_url%',
-            '$targetPathParameter' => '%security.target_path_parameter%',
-        ]);
 };
