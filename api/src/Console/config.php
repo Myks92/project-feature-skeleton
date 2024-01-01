3bc9d9678ec47c77e7c8ce1d059060f000e4d130
Maksim Vorozhtsov <myks1992@mail.ru>
2024-01-01 18:55:59 +0300

    Refactoring Console Command

---
+++ api/src/Console/config.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $configurator): void {
+    $services = $configurator
+        ->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('App\\Console\\Command\\', './Command');
+};
