dabe3dc11c9b9dd507ac59de3682963f8784c52c
Maksim Vorozhtsov <myks1992@mail.ru>
2024-03-03 02:01:46 +0300

    Fix composer unused

--- api/composer-unused.php
+++ api/composer-unused.php
@@ -8,16 +8,17 @@ use Webmozart\Glob\Glob;
 
 return static function (Configuration $config): Configuration {
     $config
-        ->setAdditionalFilesFor('icanhazstring/composer-unused', [
+        ->setAdditionalFilesFor('myks92/project-feature-skeleton', [
             __FILE__,
             ...Glob::glob(__DIR__ . '/config/*.php'),
         ]);
 
     $config->addNamedFilter(NamedFilter::fromString('symfony/property-access'));
-    $config->addNamedFilter(NamedFilter::fromString('symfony/property-info'));
     $config->addNamedFilter(NamedFilter::fromString('phpdocumentor/reflection-docblock'));
-    $config->addNamedFilter(NamedFilter::fromString('symfony/intl'));
     $config->addNamedFilter(NamedFilter::fromString('symfony/runtime'));
+    $config->addNamedFilter(NamedFilter::fromString('symfony/flex'));
+    $config->addNamedFilter(NamedFilter::fromString('symfony/http-client'));
+    $config->addNamedFilter(NamedFilter::fromString('league/flysystem-memory'));
 
     return $config;
 };
