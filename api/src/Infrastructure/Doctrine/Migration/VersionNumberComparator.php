230a8615fae6f8a3bb8390b5d86ca49105fdb9fd
Maksim Vorozhtsov <myks1992@mail.ru>
2024-03-09 19:44:42 +0300

    Add Doctrine VersionNumberComparator.php

---
+++ api/src/Infrastructure/Doctrine/Migration/VersionNumberComparator.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Doctrine\Migration;
+
+use Doctrine\Migrations\Version\Comparator;
+use Doctrine\Migrations\Version\Version;
+
+final class VersionNumberComparator implements Comparator
+{
+    public function compare(Version $a, Version $b): int
+    {
+        return strcmp($this->getNumber($a), $this->getNumber($b));
+    }
+
+    private function getNumber(Version $current): string
+    {
+        $parts = explode('\\', (string) $current);
+
+        return end($parts);
+    }
+}
--- api/src/Infrastructure/Doctrine/config.php
+++ api/src/Infrastructure/Doctrine/config.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Doctrine;
 
+use App\Infrastructure\Doctrine\Migration\VersionNumberComparator;
 use App\Infrastructure\Doctrine\Types\JsonUnescapedType;
 use Doctrine\DBAL\Types\Types as DoctrineTypes;
+use Doctrine\Migrations\Version\Comparator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $configurator): void {
@@ -17,6 +19,12 @@ return static function (ContainerConfigurator $configurator): void {
         ],
     ]);
 
+    $configurator->extension('doctrine_migrations', [
+        'services' => [
+            Comparator::class => VersionNumberComparator::class,
+        ],
+    ]);
+
     $services = $configurator
         ->services()
         ->defaults()
@@ -24,4 +32,7 @@ return static function (ContainerConfigurator $configurator): void {
         ->autoconfigure();
 
     $services->set(FixPostgreSQLDefaultSchemaListener::class);
+
+    $services->set(VersionNumberComparator::class)
+        ->tag('doctrine.migrations.dependency_factory');
 };
