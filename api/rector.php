d728b91508bea9792fa72bf195b0c3d3423d6338
Maksim Vorozhtsov <myks1992@mail.ru>
2024-03-13 02:25:30 +0300

    Migrate to new Rector config

--- api/rector.php
+++ api/rector.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRector;
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
@@ -15,79 +15,58 @@ use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\PostRector\Rector\NameImportingPostRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->parallel();
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-    $rectorConfig->cacheDirectory(__DIR__.'/var/cache/rector');
-
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/migrations',
         __DIR__ . '/src',
         __DIR__ . '/tests'
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__ . '/tests/bootstrap.php',
-        __DIR__ . '/tests/container.php',
-        ClassPropertyAssignToConstructorPromotionRector::class => [__DIR__ . '/src/Http', __DIR__ . '/src/*/Entity/*'],
-        NullToStrictStringFuncCallArgRector::class,
-        RemoveExtraParametersRector::class,
-        ReturnNeverTypeRector::class,
-        StringableForToStringRector::class,
-        NameImportingPostRector::class => [__DIR__ . '/src/*/config.php'],
-        StringClassNameToClassConstantRector::class => [__DIR__ . '/migrations'],
-        FlipTypeControlToUseExclusiveTypeRector::class,
-        DisallowedEmptyRuleFixerRector::class,
-        AddSeeTestAnnotationRector::class,
-        PreferPHPUnitThisCallRector::class,
-        LocallyCalledStaticMethodToNonStaticRector::class,
-    ]);
-
-    //Common
-    $rectorConfig->sets([
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::TYPE_DECLARATION,
-    ]);
-
-    //PHP
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_83,
-    ]);
-
-    //Doctrine
-    $rectorConfig->sets([
-        DoctrineSetList::DOCTRINE_CODE_QUALITY,
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-        DoctrineSetList::DOCTRINE_COMMON_20,
-        DoctrineSetList::DOCTRINE_DBAL_211,
-        DoctrineSetList::DOCTRINE_DBAL_30,
-        DoctrineSetList::DOCTRINE_DBAL_40,
-        DoctrineSetList::DOCTRINE_ORM_213,
-        DoctrineSetList::DOCTRINE_ORM_214,
-        DoctrineSetList::DOCTRINE_ORM_29,
-    ]);
-
-    //Symfony
-    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
-    $rectorConfig->symfonyContainerPhp(__DIR__ . '/tests/container.php');
-
-    $rectorConfig->sets([
-        SymfonySetList::SYMFONY_63,
+    ])
+    ->withCache(__DIR__ . '/var/cache/rector')
+    ->withPhpSets()
+    ->withRootFiles()
+    ->withImportNames(
+        importShortClasses: false,
+        removeUnusedImports: true
+    )
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true
+    )
+    ->withAttributesSets(
+        symfony: true,
+        doctrine: true,
+        phpunit: true
+    )
+    ->withSets([
+        SymfonySetList::SYMFONY_64,
         SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-    ]);
 
-    //PHP Unit
-    $rectorConfig->sets([
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+
         PHPUnitSetList::PHPUNIT_100,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-    ]);
-};
+        PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ])
+    ->withSkip([
+        ReturnNeverTypeRector::class,
+        AddSeeTestAnnotationRector::class,
+        StringableForToStringRector::class,
+        RemoveExtraParametersRector::class,
+        PreferPHPUnitThisCallRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        NullToStrictStringFuncCallArgRector::class,
+        FlipTypeControlToUseExclusiveTypeRector::class,
+        LocallyCalledStaticMethodToNonStaticRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class => [__DIR__ . '/src/**/Entity'],
+        NameImportingPostRector::class => [__DIR__ . '/src/Infrastructure/Notifier/config.php'],
+        StringClassNameToClassConstantRector::class => [__DIR__ . '/migrations', __DIR__ . '/src/**/Migration'],
+    ])
+    ->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml')
+    ->withSymfonyContainerPhp(__DIR__ . '/tests/container.php');
